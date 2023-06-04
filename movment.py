4788ad7beed9f6378f525dcf56e567ef3e87a0a8
Davi Roriz <127260722+DaviRorizOliveira@users.noreply.github.com>
2023-06-04 17:58:29 -0300

    movment Atualizado

--- movment.py
+++ movment.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Movimento(pygame.sprite.Sprite):
-    def __init__(self, screen):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = {
             'down': [
@@ -25,68 +25,47 @@ class Movimento(pygame.sprite.Sprite):
                 pygame.image.load('Sprites/up_03.png')
             ]
         }
+        
+        # "Hit-Box" da imagem do player
         self.TAM_HEIGHT = 47
         self.TAM_WIDTH = 25
+        
+        # Variáveis auxiliares para a velocidade do player e velocidade de mudança das sprites
         self.MOVMENT_SPEED = 200
         self.ATT_SPEED = 5
-        self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        
+        self.player_pos = pygame.Vector2(640, 360) # Posiciona o player no centro da tela
+
         self.direction = 'down'
         self.atual = 0
         self.image = self.sprites[self.direction][self.atual]
     
-    def update_sprite(self):
-        if self.direction == 'down':
-            self.atual = (self.atual + 1) % len(self.sprites[self.direction])
-            self.image = self.sprites[self.direction][self.atual]
-        elif self.direction == 'left':
-            self.atual = (self.atual + 1) % len(self.sprites[self.direction])
-            self.image = self.sprites[self.direction][self.atual]
-        elif self.direction == 'right':
-            self.atual = (self.atual + 1) % len(self.sprites[self.direction])
-            self.image = self.sprites[self.direction][self.atual]
-        elif self.direction == 'up':
-            self.atual = (self.atual + 1) % len(self.sprites[self.direction])
-            self.image = self.sprites[self.direction][self.atual]
+    def update_sprite(self, dt):
+        self.atual = (self.atual + dt * self.ATT_SPEED) % len(self.sprites[self.direction])
+        self.image = self.sprites[self.direction][int(self.atual)]
     
-    def update_pos(self, dt, screen, WIDTH, HEIGHT):
-        screen.blit(self.image, self.player_pos - pygame.Vector2(self.image.get_size()) / 2)
+    def update_pos(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.player_pos.y -= self.MOVMENT_SPEED * dt
             self.direction = 'up'
-            if self.atual >= len(self.sprites[self.direction]) - 1:
-                self.atual = 0
-            else:
-                self.atual += dt * self.ATT_SPEED
-            self.image = self.sprites[self.direction][int(self.atual)]      
+            self.update_sprite(dt)
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.player_pos.y += self.MOVMENT_SPEED * dt
             self.direction = 'down'
-            if self.atual >= len(self.sprites[self.direction]) - 1:
-                self.atual = 0
-            else:
-                self.atual += dt * self.ATT_SPEED
-            self.image = self.sprites[self.direction][int(self.atual)]
+            self.update_sprite(dt)
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.player_pos.x -= self.MOVMENT_SPEED * dt
             self.direction = 'left'
-            if self.atual >= len(self.sprites[self.direction]) - 1:
-                self.atual = 0
-            else:
-                self.atual += dt * self.ATT_SPEED
-            self.image = self.sprites[self.direction][int(self.atual)]
+            self.update_sprite(dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.player_pos.x += self.MOVMENT_SPEED * dt
             self.direction = 'right'
-            if self.atual >= len(self.sprites[self.direction]) - 1:
-                self.atual = 0
-            else:
-                self.atual += dt * self.ATT_SPEED
-            self.image = self.sprites[self.direction][int(self.atual)]
+            self.update_sprite(dt)
         
-        self.clamp_position(WIDTH, HEIGHT)
+        self.clamp_position()
         
-    def clamp_position(self, WIDTH, HEIGHT):
-        self.player_pos.x = max(self.TAM_WIDTH, min(self.player_pos.x, WIDTH - self.TAM_WIDTH))
-        self.player_pos.y = max(self.TAM_HEIGHT, min(self.player_pos.y, HEIGHT - self.TAM_HEIGHT))
+    def clamp_position(self):
+        self.player_pos.x = max(self.TAM_WIDTH, min(self.player_pos.x, 1280 - self.TAM_WIDTH))
+        self.player_pos.y = max(self.TAM_HEIGHT, min(self.player_pos.y, 720 - self.TAM_HEIGHT))
