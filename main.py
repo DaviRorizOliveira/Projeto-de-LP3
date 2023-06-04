8af41587d9e1cf5ba133483663fd7db9ab846418
Davi Roriz <127260722+DaviRorizOliveira@users.noreply.github.com>
2023-06-04 17:57:45 -0300

    main Atualizado

--- main.py
+++ main.py
@@ -17,18 +17,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Jogo')
 
-player = Movimento(screen)
+player = Movimento()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    
     screen.fill(GREEN)
-
-    player.update_pos(dt, screen, WIDTH, HEIGHT)
-
+    screen.blit(player.image, player.player_pos - pygame.Vector2(player.image.get_size()) / 2)
+    player.update_pos(dt)
     pygame.display.flip()
-
     dt = clock.tick(60) / 1000
