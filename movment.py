2563535c42d89614991691cbbe9285824adaef6b
Davi Roriz <127260722+DaviRorizOliveira@users.noreply.github.com>
2023-06-06 20:23:39 -0300

    Movimento atualizado

--- movment.py
+++ movment.py
@@ -1,4 +1,5 @@
 import pygame
+from setup import *
 
 class Movimento(pygame.sprite.Sprite):
     def __init__(self):
@@ -34,38 +35,50 @@ class Movimento(pygame.sprite.Sprite):
         self.MOVMENT_SPEED = 200
         self.ATT_SPEED = 5
         
-        self.player_pos = pygame.Vector2(640, 360) # Posiciona o player no centro da tela
+        self.player_pos = pygame.Vector2(WIDTH / 2, HEIGHT / 2) # Posiciona o player no centro da tela
 
         self.direction = 'down'
         self.atual = 0
         self.image = self.sprites[self.direction][self.atual]
     
+    def build_player(self, screen, dt, local):
+        screen.blit(self.image, self.player_pos - pygame.Vector2(self.image.get_size()) / 2)
+        self.update_pos(dt, local)
+    
     def update_sprite(self, dt):
         self.atual = (self.atual + dt * self.ATT_SPEED) % len(self.sprites[self.direction])
         self.image = self.sprites[self.direction][int(self.atual)]
     
-    def update_pos(self, dt):
+    def clamp_position(self):
+        self.player_pos.x = max(self.TAM_WIDTH, min(self.player_pos.x, WIDTH - self.TAM_WIDTH))
+        self.player_pos.y = max(self.TAM_HEIGHT, min(self.player_pos.y, HEIGHT - self.TAM_HEIGHT))
+    
+    def update_pos(self, dt, local):
         keys = pygame.key.get_pressed()
-
+        dx = 0
+        dy = 0
+        
         if keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.player_pos.y -= self.MOVMENT_SPEED * dt
+            dy = -self.MOVMENT_SPEED * dt
             self.direction = 'up'
             self.update_sprite(dt)
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.player_pos.y += self.MOVMENT_SPEED * dt
+            dy = self.MOVMENT_SPEED * dt
             self.direction = 'down'
             self.update_sprite(dt)
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            self.player_pos.x -= self.MOVMENT_SPEED * dt
+            dx = -self.MOVMENT_SPEED * dt
             self.direction = 'left'
             self.update_sprite(dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            self.player_pos.x += self.MOVMENT_SPEED * dt
+            dx = self.MOVMENT_SPEED * dt
             self.direction = 'right'
             self.update_sprite(dt)
-        
+
+        new_player_pos = self.player_pos + pygame.Vector2(dx, dy)
+
+        self.player_pos = new_player_pos
         self.clamp_position()
-        
-    def clamp_position(self):
-        self.player_pos.x = max(self.TAM_WIDTH, min(self.player_pos.x, 1280 - self.TAM_WIDTH))
-        self.player_pos.y = max(self.TAM_HEIGHT, min(self.player_pos.y, 720 - self.TAM_HEIGHT))
+
+        if local.is_collision(self):
+            local.handle_collision(self)
