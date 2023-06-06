92d1834af26fa023b1a3fb540ca8f5b29742fb25
Davi Roriz <127260722+DaviRorizOliveira@users.noreply.github.com>
2023-06-06 20:24:10 -0300

    Main atualizado

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 import pygame
+from setup import *
 from movment import Movimento
-
-# Configurações da tela
-WIDTH = 1280
-HEIGHT = 720
-
-# Configurações do jogador
-dt = 0
-
-# Cores
-GREEN = (50, 169, 62)
+from local import Local
 
 # Inicialização do pygame
 pygame.init()
@@ -18,14 +10,18 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('Jogo')
 
 player = Movimento()
+casa = Local('imagens/house.png', WIDTH - 250, 0)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+    
     screen.fill(GREEN)
-    screen.blit(player.image, player.player_pos - pygame.Vector2(player.image.get_size()) / 2)
-    player.update_pos(dt)
+    casa.build_local(screen)
+    #casa.colisão(player)
+    player.build_player(screen, dt, casa)
+    
     pygame.display.flip()
     dt = clock.tick(60) / 1000
